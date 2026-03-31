ae31416cee83334c9d0222291da773cd0a32e2fa
boratan2008-hue <boratan2008@gmail.com>
2026-03-31 10:55:29 +0200

    I set up initial data structures and points table for tournament scoring system

---
+++ scoringsystem.py
@@ -0,0 +1,12 @@
+# Tournament Scoring Sytem
+# BTEC Unit 4 Programming
+
+# storing data
+teams = {}
+individuals = {}
+team_scores = {}
+events = []
+
+# points for each place
+points = {1: 10, 2: 7, 3: 5, 4: 3, 5: 1}
+
