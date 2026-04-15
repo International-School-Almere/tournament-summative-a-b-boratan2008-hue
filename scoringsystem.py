6486b2fa65323e5121aaea686af20cc55676487e
boratan2008-hue <boratan2008@gmail.com>
2026-04-15 14:18:13 +0200

    Restart structure to regain overview. ixxed errors, add new screen for names. Buttons for save

--- scoringsystem.py
+++ scoringsystem.py
@@ -14,65 +14,58 @@ events = []
 points = {1: 10, 2: 7, 3: 5, 4: 3, 5: 1}
 
 
-# ── ADD TEAM WINDOW ──────────────────────────────────────
-def open_add_team():
-    if len(teams) >= 4:
-        messagebox.showerror("Error", "Max 4 teams allowed!")
+#add team screen
+def add_team_screen():
+    
+    #replace previous screen
+    if len(teams) >= 5:
+        messagebox.showerror("Error", "Max 5 teams allowed!")
         return
  
-    win = tk.Toplevel(root)
-    win.title("Add Team")
-    win.geometry("300x350")
-    win.config(bg="#f0f0f0")
+    #win = tk.Toplevel(root)
+    #win.title("Add Team")
+   # win.geometry("300x350")
+   # win.config(bg="#f0f0f0")
  
-    tk.Label(win, text="Team Name:", bg="#f0f0f0").pack(pady=5)
-    team_entry = tk.Entry(win, width=25)
+    tk.Label(root, text="Team Name:", bg="#f0f0f0").pack(pady=5)
+    team_entry = tk.Entry(root, width=25)
     team_entry.pack()
 
-    tk.Label(win, text="Members (one per line, max 5):, bg="#f0f0f0").pack(pady=5)
-    members_box = tk.Text(win, width=25, height=8)
-    members_box.pack()
+    Members_label = tk.Label(root, text="Members (one per line, max 5):", bg="#f0f0f0")
+    Members_label.pack(pady=5)
 
-    def save_team() :
-        name=team_entry.get().strip()
-        if name == "":
-        messagebox.showerror("Error", "Team name cant be empty")
-            return
-            if name in the teams :
-            messagebox.showerror("Error", "Team already exsists")
-            return
+    Member1 = tk.Entry(root, width=25)
+    Member1.pack()
 
-                  raw = members_box.get("1.0", tk.END).strip().split("\n")
-        members = [m.strip() for m in raw if m.strip() != ""][:5]
- 
-        teams[name] = members
-        team_scores[name] = 0
-        messagebox.showinfo("Success", f"Team '{name}' added!")
-        win.destroy()
-        refresh_leaderboard()
+    Member2 = tk.Entry(root, width=25)
+    Member2.pack()
+    
+    Member3 = tk.Entry(root, width=25)
+    Member3.pack()
 
-        tk.Button(win, text="Save Team", command=save_team, bg="#4CAF50", fg="white").pack(pady=10)
- 
- # ── ADD INDIVIDUAL WINDOW ────────────────────────────────
-def open_add_individual():
-    if len(individuals) >= 20:
-        messagebox.showerror("Error", "Max 20 individuals allowed!")
-        return
+    Member4 = tk.Entry(root, width=25)
+    Member4.pack()
 
+    Member5 = tk.Entry(root, width=25)
+    Member5.pack()
 
+    save = tk.Button(root, text="save",anchor='center',command=save_button)
+    save.pack()
 
+    back = tk.Button(root, text="return",anchor='center',command=return_button)
+    back.pack()
 
+def save_button():
+    print("save")
+    #Save info in file.
+        #read file - get data - change data - overwrite file
 
-
-
-
-
-
-
-
+def return_button():
+    print("return")
 
 
 root = tk.Tk()
 root.title("Tournament Scoring System")
 root.geometry("600x500")
-root.mainloop()
+add_team_screen()
+root.mainloop()  
