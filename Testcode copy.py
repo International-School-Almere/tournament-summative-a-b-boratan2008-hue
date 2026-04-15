6e3cb657c7a58e8a1ff2954dcb6a758013ede0f5
boratan2008-hue <boratan2008@gmail.com>
2026-04-15 14:19:36 +0200

    changes

---
+++ Testcode copy.py
@@ -0,0 +1,71 @@
+# Tournament Scoring Sytem
+# BTEC Unit 4 Programming
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+
+# storing data
+teams = {}
+individuals = {}
+team_scores = {}
+events = []
+
+# points for each place
+points = {1: 10, 2: 7, 3: 5, 4: 3, 5: 1}
+
+
+#add team screen
+def add_team_screen():
+    
+    #replace previous screen
+    if len(teams) >= 5:
+        messagebox.showerror("Error", "Max 5 teams allowed!")
+        return
+ 
+    #win = tk.Toplevel(root)
+    #win.title("Add Team")
+   # win.geometry("300x350")
+   # win.config(bg="#f0f0f0")
+ 
+    tk.Label(root, text="Team Name:", bg="#f0f0f0").pack(pady=5)
+    team_entry = tk.Entry(root, width=25)
+    team_entry.pack()
+
+    Members_label = tk.Label(root, text="Members (one per line, max 5):", bg="#f0f0f0")
+    Members_label.pack(pady=5)
+
+    Member1 = tk.Entry(root, width=25)
+    Member1.pack()
+
+    Member2 = tk.Entry(root, width=25)
+    Member2.pack()
+    
+    Member3 = tk.Entry(root, width=25)
+    Member3.pack()
+
+    Member4 = tk.Entry(root, width=25)
+    Member4.pack()
+
+    Member5 = tk.Entry(root, width=25)
+    Member5.pack()
+
+    save = tk.Button(root, text="save",anchor='center',command=save_button)
+    save.pack()
+
+    back = tk.Button(root, text="return",anchor='center',command=return_button)
+    back.pack()
+
+def save_button():
+    print("save")
+    #Save info in file.
+        #read file - get data - change data - overwrite file
+
+def return_button():
+    print("return")
+
+
+root = tk.Tk()
+root.title("Tournament Scoring System")
+root.geometry("600x500")
+add_team_screen()
+root.mainloop() 
---
+++ oldscoringsystem.py
@@ -0,0 +1,86 @@
+# Tournament Scoring Sytem
+# BTEC Unit 4 Programming
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+
+# storing data
+teams = {}
+individuals = {}
+team_scores = {}
+events = []
+
+# points for each place
+points = {1: 10, 2: 7, 3: 5, 4: 3, 5: 1}
+
+
+# ── ADD TEAM WINDOW ──────────────────────────────────────
+def open_team_screen():
+    if len(teams) >= 5:
+        messagebox.showerror("Error", "Max 5 teams allowed!")
+        return
+ 
+    win = tk.Toplevel(root)
+    win.title("Add Team")
+    win.geometry("300x350")
+    win.config(bg="#f0f0f0")
+ 
+    tk.Label(win, text="Team Name:", bg="#f0f0f0").pack(pady=5)
+    team_entry = tk.Entry(win, width=25)
+    team_entry.pack()
+
+    tk.Label(win, text="Members (one per line, max 5):", bg="#f0f0f0").pack(pady=5)
+    members_box = tk.Text(win, width=25, height=8)
+    members_box.pack()
+
+def save_team() :
+    name=team_entry.get().strip()
+    if name == "":
+       messagebox.showerror("Error", "Team name cant be empty")
+           
+    else:
+        messagebox.showerror("Error", "Team already exsists")
+
+    raw = members_box.get("1.0", tk.END).strip().split("\n")
+    members = [m.strip() for m in raw if m.strip() != ""][:5]
+ 
+    teams[name] = members
+    team_scores[name] = 0
+    messagebox.showinfo("Success", f"Team '{name}' added!")
+    win.destroy()
+    #refresh_leaderboard()
+
+    tk.Button(win, text="Save Team", command=save_team, bg="#4CAF50", fg="white").pack(pady=10)
+    
+ 
+ # ── ADD INDIVIDUAL WINDOW ────────────────────────────────
+def open_add_individual():
+    if len(individuals) >= 20:
+        messagebox.showerror("Error", "Max 20 individuals allowed!")
+        return
+
+win = tk.Toplevel(root)
+    win.title("Add Individual")
+    win.geometry("280x180")
+    win.config(bg="#f0f0f0") 
+
+    tk.Label(win, text="Participant Name:", bg="#f0f0f0").pack(pady=10)
+    name_entry = tk.Entry(win, width=25)
+    name_entry.pack() 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+root = tk.Tk()
+root.title("Tournament Scoring System")
+root.geometry("600x500")
+root.mainloop() 
